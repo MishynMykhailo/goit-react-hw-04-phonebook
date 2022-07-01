e3c8a74d7a3b210235cfcbd3a9b7c33bbd879320
MishynMykhailo <misham18092001@gmail.com>
2022-07-01 19:19:39 +0300

    Revert "first commit"
    This reverts commit afd9f5fe842ab41fa70acace0612c0b9678bba05.

--- src/components/SelectItem/SelectItem.jsx
+++
@@ -1,8 +0,0 @@
-const SelectItem = ({ props }) => {
-  return props.map(prop => (
-    <option key={prop.number} value={prop.number}>
-      {prop.code}
-    </option>
-  ));
-};
-export default SelectItem;
